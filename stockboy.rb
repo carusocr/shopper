b5390f0cabccf7b7e888dec2243d24008ecfea1f
Chris Caruso <carusocr@ldc.upenn.edu>
2014-04-29 15:16:33 -0400

    switch to capybara/poltergeist for js sites

--- stockboy.rb
+++ stockboy.rb
@@ -2,19 +2,35 @@
 
 # Working on a script that crawls supermarket pages and comparison shops for me.
 
-require 'mechanize'
+#require 'mechanize'
 require 'nokogiri'
-require 'open-uri'
+require 'capybara'
+require 'capybara/poltergeist'
 
-agent = Mechanize.new
-agent.get('http://superfreshfood.inserts2online.com/customer_Frame.jsp?drpStoreID=747&showFlash=false').search("Store").each do |page|
-	puts page.content
+#register driver
+#Capybara.register_driver :poltergeist do |app|
+#	Capybara::Poltergeist::Driver.new(app, :js_errors => false)
+#end
+Capybara.default_driver = :poltergeist
+Capybara.register_driver :poltergeist do |app|
+    options = {
+        :js_errors => true,
+        :timeout => 120,
+        :debug => false,
+        :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
+        :inspector => true,
+    }
+    Capybara::Poltergeist::Driver.new(app, options)
 end
-agent.get('http://superfreshfood.inserts2online.com/customer_Frame.jsp?drpStoreID=747&showFlash=false').search('.//*[@id="itemName0"]')
-doc = Nokogiri::HTML(open('http://superfreshfood.inserts2online.com/customer_Frame.jsp?drpStoreID=747&showFlash=false'))
-puts doc.content
 
-doc.search('Store').each do |link|
-	puts link.content
-end
-puts doc.content
+#agent.get('http://superfreshfood.inserts2online.com/customer_Frame.jsp?drpStoreID=747&showFlash=false').search("Store").each do |page|
+#       puts page.content
+#end
+#doc = Nokogiri::HTML(open('http://www.thefreshgrocer.com/Shop/WeeklyAdTextOnly.aspx'))
+#puts doc.content
+
+#doc.search('Store').each do |link|
+#       puts link.content
+#end
+
+
