7b14d5bfdb5456f391a1aadec6b4b1122ffa8c6d
carusocr <carusocr@ldc.upenn.edu>
2014-07-01 14:43:32 -0400

    added Gemfile for heroku compatibility

--- shopper.rb
+++ shopper.rb
@@ -61,7 +61,7 @@ module Shopper
       page.first(:link,'View All').click
 			sleep 2
 			# added to test window resizing
-			#page.driver.browser.manage.window.resize_to(800,800)
+			page.driver.browser.manage.window.resize_to(800,800)
       num_rows = page.find('span', :text => /Showing items 1-/).text.match(/of (\d+)/).captures
       num_rows[0].to_i.times do |meat|
         item_name =  page.find(:xpath, "//div[@id = 'itemName#{meat}']").text
@@ -69,7 +69,7 @@ module Shopper
         pricelist["#{item_name}"] = item_price
         $meaty_targets.each do |m|
           if item_name =~ /#{m}/
-           #puts "Found #{item_name} at #{storename} for #{item_price}"
+            puts "Found #{item_name} at #{storename} for #{item_price}"
             $prices << ["#{storename}","#{item_name}","#{item_price}"]
           end
         end
@@ -100,7 +100,7 @@ module Shopper
 				pricelist["#{item_name}"] = item_price
         $meaty_targets.each do |m|
           if item_name =~ /#{m}/
-  #          puts "#{storename}: #{item_name} for #{item_price}."
+            puts "#{storename}: #{item_name} for #{item_price}."
             $prices << ["#{storename}","#{item_name}","#{item_price}"]
           end
         end
@@ -117,7 +117,7 @@ module Shopper
 			  	pricelist["#{item_name}"] = item_price
           $meaty_targets.each do |m|
            if item_name =~ /#{m}/
-            #puts "#{storename}: #{item_name} for #{item_price}."
+            puts "#{storename}: #{item_name} for #{item_price}."
             $prices << ["#{storename}","#{item_name}","#{item_price}"]
            end
           end
@@ -150,4 +150,4 @@ shop.get_results(acme,acme_prices)
 shop = Shopper::APS.new
 shop.get_results(pathmark,pathmark_prices)
 shop.get_results(superfresh,superfresh_prices)
-build_table
+#build_table
