f6b3f5cacbd8e9eef45119ac551d571bdb7a0d15
Chris Caruso <carusocr@ldc.upenn.edu>
2025-03-11 14:32:28 -0700

    soupy

--- pyshop.py
+++ pyshop.py
@@ -1,4 +1,5 @@
 import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
 import random,time,os,sys
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -15,10 +16,13 @@ driver.get('https://www.safeway.com/set-store.html?storeId=1594&target=weeklyad'
 # <iframe id="0086b732-bb38-4e96-9906-38f8e46e1d0e" class="flippiframe mainframe" title="Main Panel" allowfullscreen="" scrolling="yes" frameborder="0" height="100%" width="100%" allowtransparency="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="box-sizing: content-box;"></iframe>
 #driver.switch_to.frame("0086b732-bb38-4e96-9906-38f8e46e1d0e")
 # THEN get html_source, and it works. 
-time.sleep(15)
+time.sleep(10)
 elem = driver.find_element("xpath","//iframe[@class='flippiframe mainframe']")
 driver.switch_to.frame(elem)
+html_source_code = driver.execute_script("return document.body.innerHTML;")
+html_soup: BeautifulSoup = BeautifulSoup(html_source_code, 'html.parser')
+html = html_soup.prettify("utf-8")
+with open("souptest.html", "wb") as file:
+    file.write(html)
 
 time.sleep(10)
-
-#https://stackoverflow.com/questions/7861775/python-selenium-accessing-html-source
