121af337279b4741e91ef21047b1774f6df9f870
Chris Caruso <carusocr@ldc.upenn.edu>
2025-03-11 14:27:40 -0700

    fixed safeway frame switcher

--- pyshop.py
+++ pyshop.py
@@ -11,21 +11,14 @@ chrome_options.add_argument("--ignore-certificate-errors")
 driver = uc.Chrome(options=chrome_options)
 
 #driver.get('https://www.fredmeyer.com/weeklyad')
-#driver.get('https://www.safeway.com/set-store.html?storeId=1594&target=weeklyad')
-driver.get('https://www.safeway.com/weeklyad')
+driver.get('https://www.safeway.com/set-store.html?storeId=1594&target=weeklyad')
 # <iframe id="0086b732-bb38-4e96-9906-38f8e46e1d0e" class="flippiframe mainframe" title="Main Panel" allowfullscreen="" scrolling="yes" frameborder="0" height="100%" width="100%" allowtransparency="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="box-sizing: content-box;"></iframe>
 #driver.switch_to.frame("0086b732-bb38-4e96-9906-38f8e46e1d0e")
 # THEN get html_source, and it works. 
+time.sleep(15)
 elem = driver.find_element("xpath","//iframe[@class='flippiframe mainframe']")
 driver.switch_to.frame(elem)
 
 time.sleep(10)
 
 #https://stackoverflow.com/questions/7861775/python-selenium-accessing-html-source
-#https://coupons.safeway.com/weeklyad?cmpid=lp_swy_wkad_ylp
-# https://safeway.onelink.me/kO9l/1wyhfel1
-# <iframe id="0086b732-bb38-4e96-9906-38f8e46e1d0e" class="flippiframe mainframe" title="Main Panel" allowfullscreen="" scrolling="yes" frameborder="0" height="100%" width="100%" allowtransparency="true" webkitallowfullscreen="true" mozallowfullscreen="true" style="box-sizing: content-box;"></iframe>
-# driver.switch_to.frame("0086b732-bb38-4e96-9906-38f8e46e1d0e")
-# THEN get html_source, and it works. 
-# elem = driver.find_element("xpath","//iframe[@class='flippiframe mainframe']")
-# driver.switch_to.frame(elem)
