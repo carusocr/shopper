8d8c4531806bdff86096c0eedf74a0872e7f4f5d
Chris Caruso <carusocr@ldc.upenn.edu>
2025-04-24 12:41:50 -0700

    cleanup

--- pyshop.py
+++ pyshop.py
@@ -78,14 +78,9 @@ with open("souptest.html", "wb") as file:
 with open('souptest.html') as fp:
     soup = bs(fp, features="lxml")
 
-proteins = soup.find_all("button", attrs={"aria-label": re.compile("Beef|Chicken|Pork|Tofu", re.IGNORECASE)})
+proteins = soup.find_all("button", attrs={"aria-label": re.compile("Beef|Chicken|Pork|Tofu")})
 for protein in proteins:
-    found_meat = protein.get('aria-label', None)
-    if not found_meat:
-        continue
+    found_meat = (protein['aria-label'])
     res = re.search('(?<desc>.+), ,(?<price>.+\d(\.\d+)?.+(digital_coupon)?).+?\.', found_meat)
-    if res:
-        print(res.group('desc'))
-        print(res.group('price'))
-    else:
-        print("No match found for:", found_meat)
+    print(res.group('desc'))
+    print(res.group('price'))
